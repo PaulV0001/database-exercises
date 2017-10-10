9cf31e3a281cfec6b4649869371ec7e5e67574cf
paulvaldez <valdezpaul00@gmail.com>
2017-10-10 11:34:33 -0500

    Created the albums table with mysql in the Tables exercise.

---
+++ albums_migration.sql
@@ -0,0 +1,10 @@
+USE codeup_test_db;
+CREATE TABLE albums (
+id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+artist VARCHAR(150) NOT NULL,
+name VARCHAR(150) NOT NULL,
+release_date INT,
+sales FLOAT(7,2),
+genre VARCHAR(50),
+PRIMARY KEY (id)
+);
