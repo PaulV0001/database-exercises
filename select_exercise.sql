9e26b465c4aa0cf15ed26468555a8d045253dcbf
paulvaldez <valdezpaul00@gmail.com>
2017-10-11 10:45:28 -0500

    Completed the 3 steps in the Select Exercises.

---
+++ select_exercise.sql
@@ -0,0 +1,30 @@
+USE codeup_test_db;
+SELECT 'Show all albums from Pink Floyd' AS Title;
+SELECT name
+FROM albums
+WHERE artist = 'Pink Floyd';
+
+SELECT 'The year Sgt. Pepper''s Lonely Hearts Club Band was released' AS Title;
+SELECT release_date
+FROM albums
+WHERE name = 'Sgt. Peppers Lonely Hearts Club Band';
+
+SELECT 'The genre for Nevermind' AS Title;
+SELECT genre
+FROM albums
+WHERE name = 'Nevermind';
+
+SELECT 'Which albums were released in the 1990s' AS Title;
+SELECT name
+FROM albums
+WHERE release_date BETWEEN '1990' AND '2000';
+
+SELECT 'Which albums had less than 20 million certified sales' AS Title;
+SELECT name
+FROM albums
+WHERE sales < '20.0';
+
+SELECT 'All the albums with a genre of "Rock"' AS Title;
+SELECT name
+FROM albums
+WHERE genre = 'Rock';
