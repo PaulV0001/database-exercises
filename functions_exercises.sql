dc758017eae33e65bd0bfd61c11ff3da6d362940
paulvaldez <valdezpaul00@gmail.com>
2017-10-13 09:33:12 -0500

    Completed the Functions exercise.

---
+++ functions_exercises.sql
@@ -0,0 +1,36 @@
+USE employees;
+SELECT count(*) ,gender
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+-- ORDER BY last_name, first_name
+GROUP BY gender;
+
+
+SELECT concat(first_name, last_name)
+FROM employees
+WHERE last_name LIKE 'E%'
+      OR last_name LIKE '%E'
+ORDER BY emp_no;
+
+SELECT concat(first_name, last_name)
+FROM employees
+WHERE last_name LIKE 'E%'
+      AND last_name LIKE '%E'
+ORDER BY emp_no;
+
+SELECT datediff(now(), hire_date)
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+      AND birth_date LIKE '%-12-25'
+ORDER BY birth_date, hire_date DESC;
+
+SELECT *
+FROM employees
+WHERE birth_date LIKE '%-12-25';
+
+SELECT count(*), last_name, first_name
+FROM employees
+WHERE last_name LIKE '%q%'
+      AND last_name NOT LIKE '%qu%'
+GROUP BY last_name, first_name
+ORDER BY count(*);
